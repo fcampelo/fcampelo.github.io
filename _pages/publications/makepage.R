e380b17fc4682ea53251d3931a5beb2e4737743c
fcampelo <fcampelo@ufmg.br>
2020-01-05 22:49:57 +0000

    Upload publications page (not showing)

---
+++ _pages/publications/makepage.R
@@ -0,0 +1,18 @@
+my.aliases <- list(c("Felipe Campelo",
+                     "Felipe Campelo Franca Pinto",
+                     "Felipe Campelo F. Pinto",
+                     "F.C.F. Pinto"),
+                   c("Lucas S. Batista",
+                     "Lucas Batista",
+                     "Lucas de Souza Batista",
+                     "Lucas Souza Batista"))
+lattes.list <- lattes_to_list(CV.dir         = "./",
+                              author.aliases = my.aliases)
+
+make_productions_page(lattes.list, chart.type = "rCharts",
+                      chart.width = 960, chart.height = 480, 
+                      h1.title = "Felipe Campelo", 
+                      h2.title = "Aston University", language = "EN", 
+                      which.fields = c("journal.accepted", "journal.published", 
+                                       "conference.international", "conference.national", "book.chapters", 
+                                       "books", "phd.theses", "msc.theses")) 
